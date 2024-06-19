6a532f4ba57f4c8d44cabfa795c87ff4476f0352
Danny-607 <danielhudson1294@gmail.com>
2024-06-19 10:18:44 +0100

    Updated migration with foreign key

--- database/migrations/2024_06_12_190850_create_meals_table.php
+++ database/migrations/2024_06_12_190850_create_meals_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('meals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->integer('calories');
             $table->integer('protein');
